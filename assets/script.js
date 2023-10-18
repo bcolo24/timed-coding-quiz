b732163c8a057e59b903e749b5ff5063d4081969
Bethany Colon <bethany.colon28@gmail.com>
2023-10-18 15:08:47 -0400

    added functions and const

--- assets/script.js
+++ assets/script.js
@@ -46,21 +46,76 @@ const questions = [
     }
 ];
 
-//to shuffle the questions array
-// function shuffleArray(array) {
-//     for (let i = array.length - 1; i > 0; i--) {
-//         const j = Math.floor(Math.random() * (i + 1));
-//         [array[i], array[j]] = [array[j], array[i]];
-//     }
+let currentQuestion = -1; // Initialize with -1 to indicate the title screen
+const startButton = document.getElementById("start-btn");
+const quizScreen = document.getElementById("quiz");
+const questionElement = document.getElementById("question");
+const answersElement = document.getElementById("answers");
+const nextButton = document.getElementById("next");
+const startScreen = document.getElementById("startScreen");
+
+function startQuiz() {
+    // Hide the title screen and show the quiz
+    startScreen.remove();
+    // quizScreen.append(questions);
+    // quiz.style.display = "block";
+    quizScreen.style.display = "block";
+    currentQuestion = 0;
+    showQuestion(currentQuestion);
+}
+
+startButton.addEventListener("click", function () {
+    startQuiz();
+});
+
+// Shuffle the questions array before starting the quiz
+shuffleArray(questions);
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function showQuestion(questionIndex) {
+    if (questionIndex === -1) {
+        
+    } else {
+        // Display a quiz question
+        const question = questions[questionIndex];
+        questionElement.textContent = question.question;
+        answersElement.innerHTML = "";
+
+        question.answers.forEach((answer, index) => {
+            const answerButton = document.createElement("button");
+            answerButton.textContent = answer.text;
+            answerButton.addEventListener("click", () => checkAnswer(index));
+            answersElement.appendChild(answerButton);
+        });
+    }
+}
+
+function checkAnswer(selectedIndex) {
+    // Add your logic for checking the answer here
+    // You can compare the selected answer with the correct answer
+
+    if (currentQuestion < questions.length - 1) {
+        currentQuestion++;
+        showQuestion(currentQuestion);
+    } else {
+        // Quiz is finished
+        // questionElement.textContent = "Quiz complete!";
+        // answersElement.innerHTML = "";
+        // nextButton.style.display = "none";
+    }
+}
+
+// Auto-progression to the next question without a "Next" button
+// for (let i = 0; i < questions.length; i++) {
+//     (function (questionIndex) {
+//         setTimeout(function () {
+//             showQuestion(questionIndex);
+//         }, i * 5000); // Adjust the delay (5000 milliseconds = 5 seconds) between questions
+//     })(i);
 // }
-
-//to shuffle the questions array before starting the quiz
-// shuffleArray(questions);
-
-// let currentQuestion = -1; // Initialize with -1 to indicate the title screen
-// const titleScreen = document.getElementById("title-screen");
-// const questionScreen = document.getElementById("question-screen");
-// const startButton = document.getElementById("startButton");
-// const questionElement = document.getElementById("question");
-// const answersElement = document.getElementById("answers");
-// const nextButton = document.getElementById("next");
